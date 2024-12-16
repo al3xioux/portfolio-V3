ee1d1124a21dc6b0495ee38d78a94670c8944808
Alexandre <164215444+al3xioux@users.noreply.github.com>
2024-12-17 00:13:32 +0100

    ajout doc

--- src/components/Documentation.jsx
+++ src/components/Documentation.jsx
@@ -5,10 +5,10 @@ const Documentation = () => {
     <div className='documentation'>
       <h3>Documentation</h3>
       <div className='documentation-section'>
-        <p><i class="fa-solid fa-folder"></i>  GAMEHUB</p>
-        <p><i class="fa-solid fa-folder"></i>  ZQSD</p>
-        <p><i class="fa-solid fa-folder"></i>  BIBLIO V2</p>
-        <p><i class="fa-solid fa-folder"></i>  GLPI</p>
+        <p onClick={() => window.location.href = '/doc-gamehub.zip'}><i class="fa-solid fa-folder"></i>  GAMEHUB</p>
+        <p onClick={() => window.location.href = '/doc-mind.zip'}><i class="fa-solid fa-folder"></i>  Mind Technologies</p>
+        <p onClick={() => window.location.href = '/doc-biblio.zip'}><i class="fa-solid fa-folder"></i>  BIBLIO V2</p>
+        <p onClick={() => window.location.href = '/doc-glpi.zip'}><i class="fa-solid fa-folder"></i>  GLPI</p>
       </div>
     </div>
   )
